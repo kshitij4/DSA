fdefea1b1b32ca0a157a8e08e0cfa1ac01e430d4
Kshitij Pandey <kshitijpandey444@gmail.com>
2021-02-26 13:12:11 +0530

    thomas shelby added

---
+++ edits2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int i =0;i<10;i++)
+        cout<< "shelby" << "blinders"<<endl;
+
+    cout<<"ada ran away";
+    cout<<"coppers are here"<<endl;
+    int thomas=0;
+    while(thomas * 7 != 49)
+    {
+        cout<<thomas<<" is missing";
+        thomas++;
+        cout<<endl;
+    }
+}

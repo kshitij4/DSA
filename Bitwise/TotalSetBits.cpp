a5db4a3142b34b10386cddd37b79a72de5f9774a
Kshitij Pandey <60103525+kshitij4@users.noreply.github.com>
2021-02-26 15:53:40 +0530

    Update TotalSetBits.cpp

--- Bitwise/TotalSetBits.cpp
+++ Bitwise/TotalSetBits.cpp
@@ -2,25 +2,25 @@
 
 using namespace std;
 
-int getMaxPowOf2(int n)
+int getMaxPowOf2(int n) //to get the maximum power of 2 less than n
 {
     int x=0;
-    while((1<<x) <= n)
+    while((1<<x) <= n) // while(2^x <= n)
         x++;
     return x-1;
 }
 
 int getTotalSetBits(int n)
 {
-    if(n==0)
+    if(n==0) // base case for recursive call
         return 0;
     int x = getMaxPowOf2(n);
 
-    int setTil2n = x * (1<<(x-1));
+    int setTil2n = x * (1<<(x-1));   // count of sets from 1 to 2^x
 
-    int from2nton = n -(1<<x)+1;
+    int from2nton = n -(1<<x)+1;  // set bits from 2^x to n
 
-    return ( setTil2n + from2nton + getTotalSetBits(n - (1<<x)));
+    return ( setTil2n + from2nton + getTotalSetBits(n - (1<<x)));  // returning the sum and recursive call
 }
 
 int main()

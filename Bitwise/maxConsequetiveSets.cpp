78487e784d0d329634d3c4b40b2bdd1645c61c78
Kshitij Pandey <kshitijpandey444@gmail.com>
2021-02-26 17:26:47 +0530

    more problems added

--- Bitwise/maxConsequetiveSets.cpp
+++ Bitwise/maxConsequetiveSets.cpp
@@ -23,6 +23,17 @@ int maxConsecutiveOnes(int N)
     return max;
 }
 
+int maxConsecutiveOnes2(int N)
+{
+    int count = 0; 
+    while (N!=0) 
+    { 
+        N = (N & (N << 1)); 
+        count++; 
+    } 
+    return count; 
+}
+
 int main()
 {
     int n;

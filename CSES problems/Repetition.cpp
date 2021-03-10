79e29f57c4150dd2ad7c92720cd9fd3fc122ab88
Kshitij Pandey <kshitijpandey444@gmail.com>
2021-03-10 22:47:25 +0530

    CSES problem added

---
+++ CSES problems/Repetition.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main()
+{
+    string dna;
+    cin>> dna;
+    int Maxlen = 1;
+    int len = 1;
+    for(int i=1;i<dna.size();i++)
+    {
+        if(dna[i-1]==dna[i])
+            len++;
+        else
+            len=1;
+        Maxlen = max(len,Maxlen);
+    }
+
+    cout<< Maxlen;
+    return 0;
+}

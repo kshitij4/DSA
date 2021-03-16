b57dc66c3524f6eab5337089ba112e4517ed0ba1
Kshitij Pandey <kshitijpandey444@gmail.com>
2021-03-16 19:48:31 +0530

    Recusion problem gfg added

---
+++ GFG problems/Recursion/LuckyNumber.cpp
@@ -0,0 +1,22 @@
+//A No is lucky if it is not deleted as part of the process:
+// The process starts with deletion of every 2nd element , then 3rd and so on.
+# include<bits/stdc++.h>
+
+using namespace std;
+
+bool isLucky(int n,int i=2) 
+{
+    if(n<i)
+        return true;
+    if(n%i == 0)
+        return false;
+    return isLucky(n-(n/i),i+1);
+}
+int main()
+{
+    int n;
+    cin>>n;
+    cout<< isLucky(n);
+
+    return 0; 
+}

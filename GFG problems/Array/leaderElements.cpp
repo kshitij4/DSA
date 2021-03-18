a46384efd6f76501c021db5d316f6d6b33681815
Kshitij Pandey <kshitijpandey444@gmail.com>
2021-03-18 21:50:23 +0530

    array problem gfg added

---
+++ GFG problems/Array/leaderElements.cpp
@@ -0,0 +1,37 @@
+//to find leaders in an array
+//an element is a leader if every element on its right is smaller
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> getLeaders(int a[],int n)
+{
+    vector<int> res;
+    int currLeader = a[n-1];
+    res.push_back(currLeader);
+    for(int i=n-2;i>=0;i--)
+    {
+        if(a[i]>currLeader)
+        {
+            res.push_back(a[i]);
+            currLeader = a[i];
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+
+    vector<int> leader = getLeaders(arr,n);
+
+    for(int i=leader.size()-1;i>=0;i--)
+        cout<<leader[i]<<" ";
+
+    return 0;
+}
